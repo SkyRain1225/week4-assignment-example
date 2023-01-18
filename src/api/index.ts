13db1df1a7f0d2ee4691267c45d461250c0f9af4
SkyRain1225 <rudwns9286@gmail.com>
2023-01-19 06:59:51 +0900

    ✨ Axios API 추가

---
+++ src/api/index.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { IComment } from "../types";
+
+const baseURL = "http://localhost:4000";
+
+export const instance = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const getCommentsAPI = async () => {
+  try {
+    const { data } = await instance.get<IComment[]>("/comments");
+    return data;
+  } catch {
+    console.log("get failed");
+  }
+};
+
+export const postCommentAPI = async (comment: IComment) => {
+  try {
+    const { data } = await instance.post<IComment>("/comments", comment);
+    return data;
+  } catch {
+    console.log("post failed");
+  }
+};
+
+export const deleteCommentAPI = async (id: number) => {
+  try {
+    const { data } = await instance.delete<IComment>(`/comments/${id}`);
+    return data;
+  } catch {
+    console.log("delete failed");
+  }
+};
+
+// export const updateCommentAPI = async (comment: IComment) => {
+//   const { data } = await instance.put<IComment>(
+//     `/comments/${comment.id}`,
+//     comment,
+//   );
+//   return data;
+// };
