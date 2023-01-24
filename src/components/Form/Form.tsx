c7eee12260308093e8102f01ac97603447ad3bd0
SkyRain1225 <rudwns9286@gmail.com>
2023-01-24 15:43:54 +0900

    ✨ react-hook-form / create 구현

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,22 +1,61 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useRecoilState } from "recoil";
+import { CommentState } from "../../states/CommentState";
+import { postCommentAPI } from "../../api";
+import { IComment } from "../../types";
 import * as S from "./index";
 
 function Form() {
+  const [commentList, setCommentList] = useRecoilState(CommentState);
+
+  const { register, handleSubmit, reset } = useForm<IComment>();
+
+  const onSubmit: SubmitHandler<IComment> = (data) => {
+    postCommentAPI(data).then((res) => {
+      if (res) {
+        setCommentList([...commentList, res]);
+        reset();
+      }
+    });
+  };
+
   return (
     <S.FormStyle>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
-          name="profile_url"
+          id="profile_url"
           placeholder="https://picsum.photos/id/1/50/50"
+          autoComplete="off"
           required
+          {...register("profile_url")}
         />
         <br />
-        <input type="text" name="author" placeholder="작성자" />
+        <input
+          type="text"
+          id="author"
+          placeholder="작성자"
+          autoComplete="off"
+          {...register("author")}
+        />
         <br />
-        <textarea name="content" placeholder="내용" required />
+        <textarea
+          id="content"
+          placeholder="내용"
+          autoComplete="off"
+          required
+          {...register("content")}
+        />
         <br />
-        <input type="text" name="createdAt" placeholder="2020-05-30" required />
+        <input
+          type="text"
+          id="createdAt"
+          placeholder="2020-05-30"
+          autoComplete="off"
+          required
+          {...register("createdAt")}
+        />
         <br />
         <button type="submit">등록</button>
       </form>
